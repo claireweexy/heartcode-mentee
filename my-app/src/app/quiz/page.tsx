e6538afc11e073d9c63296fdd2aa28d9a41563c8
lineonthepaper <claireweexy@gmail.com>
2024-12-10 14:09:00 +0800

    title

--- my-app/src/app/quiz/page.tsx
+++ my-app/src/app/quiz/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
     return (
         <div>
-            
+            <div>
+                Drugs aren't good. They're not food.
+                <br/>
+                
+            </div>
         </div>
     );
 }

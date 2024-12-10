ec2690a7f323420c6448ecdfe016aa45777eb081
lineonthepaper <claireweexy@gmail.com>
2024-12-10 13:50:57 +0800

    text edit

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -11,23 +11,22 @@ import {
 import Healthy_Man from "@/app/assets/healthy_man.jpg";
 import Drug from "@/app/assets/drug.jpg";
 import New_Drug_Addict from "@/app/assets/new_drug_addict.webp";
-import Cocaine from "@/app/assets/cocaine_bags.jpg";
 import {Button, buttonVariants} from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="flex justify-center bg-repeat" style={{backgroundImage: `url("${Cocaine}")`}}>
+    <div className="flex justify-center">
       <div className="text-center p-1 w-60 font-serif hover:font-sans hover:bg-sky-100">
         <div>
           please do <b>drugs</b>
         </div>
       </div>
-      <div>
-        <Carousel className="w-80">
+      <div className="flex justify-center">
+        <Carousel className="w-6/12">
           <CarouselContent>
-            <CarouselItem><Image src={Healthy_Man} height={300} alt="Healthy man"/></CarouselItem>
-            <CarouselItem><Image src={Drug} height={300} alt="drug"/></CarouselItem>
-            <CarouselItem><Image src={New_Drug_Addict} height={300} alt="new drug addict"/><div className="font-extrabold text-6xl">HE HE HA HA!!</div></CarouselItem>
+            <CarouselItem><Image height={300} src={Healthy_Man} alt="Healthy man"/></CarouselItem>
+            <CarouselItem><Image height={300} src={Drug} alt="drug"/></CarouselItem>
+            <CarouselItem><Image height={300} src={New_Drug_Addict} alt="new drug addict"/><div className="font-extrabold text-3xl">(if you wanna die)</div></CarouselItem>
           </CarouselContent>
           <CarouselPrevious />
           <CarouselNext />

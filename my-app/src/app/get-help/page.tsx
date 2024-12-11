6e06cfb9a590e26e1a928f8162866a036ca83e04
lineonthepaper <claireweexy@gmail.com>
2024-12-11 12:43:43 +0800

    fix apostrophe

--- my-app/src/app/get-help/page.tsx
+++ my-app/src/app/get-help/page.tsx
@@ -11,7 +11,7 @@ export default function GetHelp() {
                 
                 <br/>
                 <div className="text-3xl px-80 flex justify-center">
-                    Addiction is not something that should be faced in silence. If you or someone you care about is struggling with the difficult journey of overcoming drug addiction, it's crucial to reach out for support. You're not alone in this fight—there are trusted friends, family, and professionals who want to help you. Remember, seeking help is a sign of strength, not weakness. No matter how dark it may seem, there is always hope, and recovery is possible. There is light at the end of the tunnel, and you deserve to find it.
+                    {"Addiction is not something that should be faced in silence. If you or someone you care about is struggling with the difficult journey of overcoming drug addiction, it's crucial to reach out for support. You're not alone in this fight—there are trusted friends, family, and professionals who want to help you. Remember, seeking help is a sign of strength, not weakness. No matter how dark it may seem, there is always hope, and recovery is possible. There is light at the end of the tunnel, and you deserve to find it."}
                 </div>
                 <br/>
                 

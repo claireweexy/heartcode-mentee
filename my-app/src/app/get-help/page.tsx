89cf1df2c80e8c83855ff691e8145fd4e8a53ee5
lineonthepaper <claireweexy@gmail.com>
2024-12-11 14:43:46 +0800

    links

--- my-app/src/app/get-help/page.tsx
+++ my-app/src/app/get-help/page.tsx
@@ -1,4 +1,5 @@
 import { FlipWords } from "@/components/ui/flip-words";
+import Link from "next/link";
 
 export default function GetHelp() {
     const words: string[] = ["should not be abused", "are addictive", "are dangerous", "ruin lives", "break families", "kill"]
@@ -14,7 +15,20 @@ export default function GetHelp() {
                     {"Addiction is not something that should be faced in silence. If you or someone you care about is struggling with the difficult journey of overcoming drug addiction, it's crucial to reach out for support. You're not alone in this fight—there are trusted friends, family, and professionals who want to help you. Remember, seeking help is a sign of strength, not weakness. No matter how dark it may seem, there is always hope, and recovery is possible. There is light at the end of the tunnel, and you deserve to find it."}
                 </div>
                 <br/>
-                
+                <div className="text-3xl font-bold">
+                    Resources
+                </div>
+                <Link legacyBehavior href="https://wecare.org.sg/addiction-drug.php">
+                    <a target="_blank" rel="noopener noreferrer">We Care</a>
+                </Link>
+                <br/>
+                <Link legacyBehavior href="https://addictionsrecovery.sg/services">
+                    <a target="_blank" rel="noopener noreferrer">Addictions Recovery</a>
+                </Link>
+                <br/>
+                <Link legacyBehavior href="https://imh.com.sg/Clinical-Services/Specialised-Services/Pages/Addictions-Management.aspx">
+                    <a target="_blank" rel="noopener noreferrer">Institute of Mental Health</a>
+                </Link>
             </div>
         </div>
     );

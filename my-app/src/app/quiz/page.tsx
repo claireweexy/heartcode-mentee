e1060fcbb1122bbadad00ce0c74bf1de51ec2464
lineonthepaper <claireweexy@gmail.com>
2024-12-11 12:39:27 +0800

    subway surfers and centre text on get help

--- my-app/src/app/quiz/page.tsx
+++ my-app/src/app/quiz/page.tsx
@@ -1,11 +1,15 @@
 export default function Quiz() {
     return (
-        <div>
-            <div>
+        <div className="bg-[url('/assets/dont_take_drugs.png')] h-screen">
+            <div className="bg-opacity-90 bg-white text-center">
                 {"Drugs aren't good. They're not food."}
                 <br/>
                 
             </div>
+            <div className="fixed bottom-0 left-0 border-black border-2">
+                <img src={"/assets/subway_surfers.gif"} alt={"Subway Surfers"} style={{height: '300px', width: 'auto'}}/>
+            </div>
+            
         </div>
     );
 }

5c3fcb80af2a24748d780dacc1f8ceb9c873ce4d
lineonthepaper <claireweexy@gmail.com>
2024-12-12 16:40:19 +0800

    gearg

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,44 +1,67 @@
 "use client"
 // import Image from "next/image";
-import React from "react"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import React, { useEffect } from "react";
+import { Carousel, Card } from "@/components/ui/apple-cards-carousel";
 
-import { mainBank } from "@/components/imagebank";
+const data = [
+  {
+    category: "Purpose",
+    title: "What is this website about?",
+    src: "/assets/new_drug_addict.webp",
+    content: ``,
+  },
+  {
+    category: "Statistics",
+    title: "Statistics on substance abuse",
+    src: "/assets/new_drug_addict.webp",
+    content: ``,
+  },
+  {
+    category: "News",
+    title: "Find out more about substance abuse.",
+    src: "/assets/new_drug_addict.webp",
+    content: ``,
+  },
+ 
+  {
+    category: "Quotes",
+    title: "Hear from past addicts",
+    src: "/assets/new_drug_addict.webp",
+    content: ``,
+  },
+];
 
 export default function Home() {
+  const cards = data.map((card, index) => (
+    <Card key={card.src} card={card} index={index} />
+  ));
+
   return (
     <div>
-      <div className="p-1 font-serif hover:font-sans">
-        <div className="text-center">
-          please do <b>drugs</b>. be like him
+      <div className="p-6">
+        <div className="text-2xl font-semibold">Substance Abuse</div>
+        <div className="text-lg px-6">Do you know?</div>
+        <div className="px-6">The Oxford definition of substance abuse: 
+          <p className="px-6">The habit of taking too much harmful drugs or drinking too much alcohol</p>
         </div>
+        <br />
+        <div className="text-lg px-6">But do you know?</div>
+        <div className="px-6">Abusing substances are more dangerous. They break families, ruin lives...</div>
       </div>
-      <div className="flex justify-center">
-        <Carousel className="w-6/12">
-          <CarouselContent>
-            {mainBank.map((image, index) => (
-              <CarouselItem key={index}>
-                <div key={index} className="flex justify-center">
-                  <img key={index} src={image.url} alt={image.name} style={{height: '300px', width: 'auto'}}/>
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel>
+      <div className="p-6">
+        <div className="text-2xl font-semibold">Victim Rememberance Day</div>
+        <div className="px-6">In honour of the lives that were ruined due to drugs, Singapore has designated <b>every third of friday of May</b> each year as Drug Victims Rememberance Day...</div>
+      </div>
+      <div className="w-full h-full py-20">
+        <h2 className="pl-4 mx-auto text-xl md:text-5xl font-bold text-neutral-800 dark:text-neutral-200 font-sans">
+          More Information
+        </h2>
+        <Carousel items={cards} />
       </div>
       <br/>
       <div className="flex justify-center">
         <img src={"/assets/see_drugs.png"} alt={"See_drugs"} style={{height: '300px', width: 'auto'}}/>
       </div>
-      
     </div>
   );
 }

e646a00d22346c40974570923913d05924d04077
Wilfred Wee <wilfredwee@WILFRED-13inPRO-9.local>
2024-12-12 14:34:40 +0800

    text

--- my-app/src/app/about-drug-addicts/page.tsx
+++ my-app/src/app/about-drug-addicts/page.tsx
@@ -3,57 +3,50 @@ import { AnimatedTestimonials } from "@/components/ui/animated-testimonials";
 export default function DrugAddicts() {
   const testimonials = [
     {
-    quote:`Juice WRLD, born Jarad Higgins, was a talented rapper and singer known for his emotional lyrics and hits like Lucid Dreams. His music, had themes of love, heartbreak, and mental health, deeply resonated with fans, especially those facing similar issues. Despite success, Jarad battled substance abuse and mental health issues, often referencing his struggles in his songs. In December 2019, he tragically passed away at just 21 from a drug overdose, shocking the music world and shedding light on the dangers of addiction and the pressures of fame.
-  Juice WRLD's story highlights the importance of mental health awareness and the need for support for those battling addiction. His music continues to inspire and provide comfort, leaving a lasting legacy.
+    quote:`Juice WRLD was a rapper and singer known for his emotional lyrics and hits like Lucid Dreams. Despite success, he battled against substance abuse and mental health issues, often referencing his struggles in his songs. In December 2019, he tragically passed away at just 21 from a drug overdose, shocking the music world and shedding light on the dangers of addiction and the pressures of fame.
+Juice WRLD's story highlights the importance of mental health awareness and the need for support for those battling addiction.
     `,
       name: "Juice World",
       designation: "American rapper and singer",
       src: "/assets/juice world.jpg",
       story: `
-These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person's struggle can fracture families, devastate friendships, and leave entire communities grappling with grief. They remind us that addiction is not a solitary battle; it pulls everyone into its orbit, leaving scars that last a lifetime.
-Yet, within these stories also lies an important message: the need for compassion, understanding, and action. They remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories are here not only to mourn the lives lost and broken but to inspire us to do better—for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope. Let's make our voices louder than the silence of lives ruined, and let the light of our support shine brighter than the darkness of addiction.
+These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can affect relationships, ruin lives. They remind us that addiction is not a battle that needs to be comquered alone. These stories remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories not only mourn the lives lost and broken but also inspire us to do better for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope.
 `,
     },
     {
       quote:
       `Liam Payne, a member of One Direction, has openly shared his struggles with addiction and mental health. Behind the fame and chart-topping hits, he battled substance abuse, driven by the pressures of success and personal insecurities.
-Liam's addiction worsened during One Direction's rise, affecting his mental and emotional well-being. Seeking help and therapy marked the start of his recovery, allowing him to regain control of his life and prioritize his mental health.
-His journey from struggle to redemption highlights the importance of seeking support and raising awareness about addiction and mental health challenges. Liam’s story inspires others to believe that recovery is always possible.
+Liam's addiction worsened during One Direction's rise, affecting his mental and emotional well-being. After intaking cocaine and alchohol, Payne fell from his room's balcony on the third floor of his hotel in the upscale neighborhood of Palermo, in the Argentine capital. His autopsy said he died from internal and external hemorrhage.
 `,
       name: "Liam Payne",
       designation: "Member of the globally successful group One Direction",
       src: "/assets/liam payne.webp",
-      story:`
-These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can fracture families, devastate friendships, and leave entire communities grappling with grief. They remind us that addiction is not a solitary battle; it pulls everyone into its orbit, leaving scars that last a lifetime.
-Yet, within these stories also lies an important message: the need for compassion, understanding, and action. They remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories are here not only to mourn the lives lost and broken but to inspire us to do better—for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope. Let’s make our voices louder than the silence of lives ruined, and let the light of our support shine brighter than the darkness of addiction.
+      story:
+`These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can affect relationships, ruin lives. They remind us that addiction is not a battle that needs to be comquered alone. These stories remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories not only mourn the lives lost and broken but also inspire us to do better for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope.
 `,
       
     },
     {
       quote:
-      `Mac Miller, born Malcolm James McCormick, was an American rapper known for his heartfelt lyrics and relatable music. Rising to fame with mixtapes like K.I.D.S. and Best Day Ever, he connected with fans through his reflections on life, love, and ambition.
-    Despite his success, Mac battled addiction and mental health struggles, which became evident over time. The pressures of fame and personal challenges led to his tragic passing from an accidental drug overdose in September 2018.
-    Mac Miller’s story is a powerful reminder of the dangers of addiction and the importance of mental health. Though he is gone, his music and legacy continue to inspire, highlighting the need to seek help and support during life’s hardest moments.
+      `Mac Miller, was an American rapper. However, Mac battled addiction and mental health struggles. The pressures of fame and personal challenges led to his tragic passing from an accidental drug overdose in September 2018. Mac Miller’s story is a powerful reminder of the dangers of addiction and the importance of mental health. Though he is gone, his music and legacy continue to inspire, highlighting the need to seek help and support during life’s hardest moments.
 `,
       name: "Mac Miller",
       designation: "American rapper and singer",
       src: "/assets/mac 3.png",
-      story: `These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can fracture families, devastate friendships, and leave entire communities grappling with grief. They remind us that addiction is not a solitary battle; it pulls everyone into its orbit, leaving scars that last a lifetime.
-Yet, within these stories also lies an important message: the need for compassion, understanding, and action. They remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories are here not only to mourn the lives lost and broken but to inspire us to do better—for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope. Let’s make our voices louder than the silence of lives ruined, and let the light of our support shine brighter than the darkness of addiction.
+      story: `These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can affect relationships, ruin lives. They remind us that addiction is not a battle that needs to be comquered alone. These stories remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories not only mourn the lives lost and broken but also inspire us to do better for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope.
 
 `,
     },
     {
       quote:
-`Sarah was a compassionate 24-year-old with a bright future, but her life took a tragic turn when she started dating someone involved in drugs. Though she initially resisted, she was eventually pressured into trying heroin, which led to a downward spiral she couldn’t escape.
+      `Sarah was a bright 24-year-old with a bright future, but her life took a tragic turn when she started dating someone involved in drugs. Though she initially resisted, she was eventually pressured into trying heroin, which led to a downward spiral she couldn’t escape.
 A few months later, Sarah tragically passed away from a heroin overdose outside a nightclub, leaving her family and friends devastated. They had watched her change, unable to break free from addiction's grip.
 Sarah’s story is a heartbreaking reminder that addiction affects not just the user but everyone around them, leaving lasting pain and loss in its wake.
 `,
       name: "Sarah",
       designation: "24 Year Old Female",
       src: "/assets/secret.jpg",
-      story: `These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can fracture families, devastate friendships, and leave entire communities grappling with grief. They remind us that addiction is not a solitary battle; it pulls everyone into its orbit, leaving scars that last a lifetime.
-Yet, within these stories also lies an important message: the need for compassion, understanding, and action. They remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories are here not only to mourn the lives lost and broken but to inspire us to do better—for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope. Let’s make our voices louder than the silence of lives ruined, and let the light of our support shine brighter than the darkness of addiction.
+      story: `These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can affect relationships, ruin lives. They remind us that addiction is not a battle that needs to be comquered alone. These stories remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories not only mourn the lives lost and broken but also inspire us to do better for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope.
 
 `,
     },
@@ -66,19 +59,18 @@ Through therapy, Nicole focused on healing and accepted she couldn’t control h
       name: "Nicole Richie ",
       designation: "American fashion designer and actress",
       src: "/assets/nicole.png",
-      story: ""
+      story: "These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can affect relationships, ruin lives. They remind us that addiction is not a battle that needs to be comquered alone. These stories remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories not only mourn the lives lost and broken but also inspire us to do better for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope."
     },
     {
       quote:
-`Jake, a 32-year-old from a small town, shared a close bond with his older brother Aaron, whose life took a dark turn when he began experimenting with drugs in his late teens. Casual use quickly spiraled into addiction, and Jake watched helplessly as Aaron’s health and relationships crumbled.
+`Jake's older brother Aaron's life took a dark turn when he began experimenting with drugs in his late teens. Casual use quickly spiraled into addiction, and Jake watched helplessly as Aaron’s health and relationships crumbled.
 Jake tried everything to help, from offering support to driving Aaron to rehab multiple times, but each relapse brought deeper frustration and heartache. “Watching someone you love lose themselves to addiction is one of the hardest things you can go through,” Jake later shared.
 After a near-fatal overdose, Aaron had a wake-up call and committed to getting sober. Jake stood by him throughout the difficult journey, helping rebuild trust and heal their family’s wounds. Reflecting on their experience, Jake said, “It wasn’t just about him getting clean—it was about love, patience, and second chances.”
 `,
       name: "Jake",
       designation: "32 Year Old Male",
       src: "/assets/secret.jpg",
-      story: `These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can fracture families, devastate friendships, and leave entire communities grappling with grief. They remind us that addiction is not a solitary battle; it pulls everyone into its orbit, leaving scars that last a lifetime.
-Yet, within these stories also lies an important message: the need for compassion, understanding, and action. They remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories are here not only to mourn the lives lost and broken but to inspire us to do better—for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope. Let’s make our voices louder than the silence of lives ruined, and let the light of our support shine brighter than the darkness of addiction.
+      story: `These accounts are more than just cautionary tales; they are a stark wake-up call. They show us the ripple effect of addiction—how one person’s struggle can affect relationships, ruin lives. They remind us that addiction is not a battle that needs to be comquered alone. These stories remind us of the importance of speaking up, of offering help, and of creating a world where those struggling with addiction feel supported, not judged. These stories not only mourn the lives lost and broken but also inspire us to do better for ourselves, for our loved ones, and for the countless others who are still fighting their battles. Together, we can break the cycle and choose a path of healing, love, and hope.
 
 `,
     },
